108c2bff9190bd130e194a1e6681895321d133b1
Mathis Chenuet <artdevelopp@hotmail.fr>
2018-11-10 16:17:30 +0100

    typo

--- tokenization.py
+++ tokenization.py
@@ -89,7 +89,7 @@ def convert_tokens_to_ids(vocab, tokens):
 
 
 def whitespace_tokenize(text):
-  """Runs basic whitespace cleaning and splitting on a peice of text."""
+  """Runs basic whitespace cleaning and splitting on a piece of text."""
   text = text.strip()
   if not text:
     return []
