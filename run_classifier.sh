b28173da725c68dfdc13e83801a1993e08daec62
henan.wang <henan.wang@henan.wang's MacBook Pro>
2018-11-14 18:34:34 +0800

    add script

---
+++ run_classifier.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+export BERT_BASE_DIR=/Users/henan.wang/Documents/workspace/dataset/bert_model/uncased_L-12_H-768_A-12
+export GLUE_DIR=/Users/henan.wang/Documents/workspace/dataset/glue_data
+
+python run_classifier.py \
+  --task_name=MRPC \
+  --do_train=true \
+  --do_eval=true \
+  --data_dir=$GLUE_DIR/MRPC \
+  --vocab_file=$BERT_BASE_DIR/vocab.txt \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+  --max_seq_length=128 \
+  --train_batch_size=32 \
+  --learning_rate=2e-5 \
+  --num_train_epochs=3.0 \
+  --output_dir=/tmp/mrpc_output/
