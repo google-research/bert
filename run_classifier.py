ed75506c6d0873d51f174132d201e01aa5e04737
孙树兵 <291952004@qq.com>
2018-12-07 15:00:42 +0800

    添加中文文本相似训练数据预处理

--- run_classifier.py
+++ run_classifier.py
@@ -354,6 +354,77 @@ class ColaProcessor(DataProcessor):
           InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
     return examples
 
+class SelfProcessor(DataProcessor):
+    """Processor for the CoLA data set (GLUE version)."""
+
+    def get_train_examples(self, data_dir):
+        file_path = os.path.join(data_dir, 'train.csv')
+        with open(file_path, 'r', encoding="utf-8") as f:
+            reader = f.readlines()
+        examples = []
+        for index, line in enumerate(reader):
+            guid = 'train-%d' % index
+            split_line = line.strip().split("\t")
+            print(split_line)
+            text_a = tokenization.convert_to_unicode(split_line[1])
+            text_b = tokenization.convert_to_unicode(split_line[2])
+            label = split_line[3]
+            examples.append(InputExample(guid=guid, text_a=text_a,
+                                         text_b=text_b, label=label))
+        return examples
+
+    def get_dev_examples(self, data_dir):
+        file_path = os.path.join(data_dir, 'val.csv')
+        with open(file_path, 'r', encoding="utf-8") as f:
+            reader = f.readlines()
+        examples = []
+        for index, line in enumerate(reader):
+            guid = 'train-%d' % index
+            split_line = line.strip().split("\t")
+            text_a = tokenization.convert_to_unicode(split_line[1])
+            text_b = tokenization.convert_to_unicode(split_line[2])
+            label = split_line[3]
+            examples.append(InputExample(guid=guid, text_a=text_a,
+                                         text_b=text_b, label=label))
+        return examples
+
+    def get_test_examples(self, data_dir):
+        """See base class."""
+        file_path = os.path.join(data_dir, 'test.csv')
+        with open(file_path, 'r', encoding="utf-8") as f:
+            reader = f.readlines()
+        examples = []
+        for index, line in enumerate(reader):
+            guid = 'train-%d' % index
+            split_line = line.strip().split("\t")
+            text_a = tokenization.convert_to_unicode(split_line[1])
+            text_b = tokenization.convert_to_unicode(split_line[2])
+            label = split_line[3]
+            examples.append(InputExample(guid=guid, text_a=text_a,
+                                         text_b=text_b, label=label))
+        return examples
+
+    def get_labels(self):
+        """See base class."""
+        return ["0", "1"]
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for (i, line) in enumerate(lines):
+            # Only the test set has a header
+            if set_type == "test" and i == 0:
+                continue
+            guid = "%s-%s" % (set_type, i)
+            if set_type == "test":
+                text_a = tokenization.convert_to_unicode(line[1])
+                label = "0"
+            else:
+                text_a = tokenization.convert_to_unicode(line[3])
+                label = tokenization.convert_to_unicode(line[1])
+            examples.append(
+                InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+        return examples
 
 def convert_single_example(ex_index, example, label_list, max_seq_length,
                            tokenizer):
@@ -746,6 +817,7 @@ def main(_):
       "mnli": MnliProcessor,
       "mrpc": MrpcProcessor,
       "xnli": XnliProcessor,
+      "similarity": SelfProcessor  # 添加自己的processor
   }
 
   if not FLAGS.do_train and not FLAGS.do_eval and not FLAGS.do_predict:
