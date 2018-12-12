eca7b0d77ad40d0f03775faff3a11a9251fb2e21
zhao meng <1078885958@qq.com>
2018-12-12 11:41:47 +0800

    delete the unnecessary "as"

--- run_classifier.py
+++ run_classifier.py
@@ -393,7 +393,7 @@ def convert_single_example(ex_index, example, label_list, max_seq_length,
   # it easier for the model to learn the concept of sequences.
   #
   # For classification tasks, the first vector (corresponding to [CLS]) is
-  # used as as the "sentence vector". Note that this only makes sense because
+  # used as the "sentence vector". Note that this only makes sense because
   # the entire model is fine-tuned.
   tokens = []
   segment_ids = []
