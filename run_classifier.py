4f9ead6593038bc1b8aeaa9e50fe3899ba0fbffd
Leonardo Apolonio <apolonio.leonardo@gmail.com>
2018-12-26 12:34:10 -0500

    add do_serve option to script
    allowing for export of model in an tensorflow compliant manner

--- run_classifier.py
+++ run_classifier.py
@@ -75,6 +75,10 @@ flags.DEFINE_bool(
     "do_predict", False,
     "Whether to run the model in inference mode on the test set.")
 
+flags.DEFINE_bool(
+    "do_serve", False,
+    "Whether to export the built model.")
+
 flags.DEFINE_integer("train_batch_size", 32, "Total batch size for training.")
 
 flags.DEFINE_integer("eval_batch_size", 8, "Total batch size for eval.")
@@ -619,7 +623,7 @@ def model_fn_builder(bert_config, num_labels, init_checkpoint, learning_rate,
           return tf.train.Scaffold()
 
         scaffold_fn = tpu_scaffold
-      else:
+      elif not FLAGS.do_serve:
         tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
 
     tf.logging.info("**** Trainable Variables ****")
@@ -748,9 +752,9 @@ def main(_):
       "xnli": XnliProcessor,
   }
 
-  if not FLAGS.do_train and not FLAGS.do_eval and not FLAGS.do_predict:
+  if not FLAGS.do_train and not FLAGS.do_eval and not FLAGS.do_predict and not FLAGS.do_serve:
     raise ValueError(
-        "At least one of `do_train`, `do_eval` or `do_predict' must be True.")
+        "At least one of `do_train`, `do_eval` or `do_predict' or `do_serve` must be True.")
 
   bert_config = modeling.BertConfig.from_json_file(FLAGS.bert_config_file)
 
@@ -902,6 +906,25 @@ def main(_):
             str(class_probability) for class_probability in prediction) + "\n"
         writer.write(output_line)
 
+  if FLAGS.do_serve:
+    def serving_input_fn():
+      with tf.variable_scope("foo"):
+        feature_spec = {
+          "input_ids": tf.FixedLenFeature([FLAGS.max_seq_length], tf.int64),
+          "input_mask": tf.FixedLenFeature([FLAGS.max_seq_length], tf.int64),
+          "segment_ids": tf.FixedLenFeature([FLAGS.max_seq_length], tf.int64),
+          "label_ids": tf.FixedLenFeature([], tf.int64),
+        }
+        serialized_tf_example = tf.placeholder(dtype=tf.string,
+                                               shape=[None],
+                                               name='input_example_tensor')
+        receiver_tensors = {'examples': serialized_tf_example}
+        features = tf.parse_example(serialized_tf_example, feature_spec)
+        return tf.estimator.export.ServingInputReceiver(features, receiver_tensors)
+
+    estimator._export_to_tpu = False  # this is important
+    path = estimator.export_savedmodel('export_t', serving_input_fn)
+    print("Saved to:", path)
 
 if __name__ == "__main__":
   flags.mark_flag_as_required("data_dir")
