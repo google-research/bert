8f16e4d4bf15c58f8382e8aba074970124e56f79
Thor Johnsen <tjohnsen@nvidia.com>
2019-02-13 12:45:00 -0800

    Relying on upstream having fixed layer norm epsilon for fp16

--- modeling.py
+++ modeling.py
@@ -363,7 +363,8 @@ def dropout(input_tensor, dropout_prob):
 
 def layer_norm(input_tensor, name=None):
   """Run layer normalization on the last dimension of the tensor."""
-  return tf.contrib.layers.layer_norm(inputs=input_tensor, begin_norm_axis=-1, begin_params_axis=-1, scope=name)
+  return tf.contrib.layers.layer_norm(
+      inputs=input_tensor, begin_norm_axis=-1, begin_params_axis=-1, scope=name)
 
 
 def layer_norm_and_dropout(input_tensor, dropout_prob, name=None):
