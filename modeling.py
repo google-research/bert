5c5e3f6c0ac510a78bd77b0ef13bb44f96596fe1
unknown <btxlaws@126.com>
2019-08-14 10:36:22 +0800

    support resue bert variables

--- modeling.py
+++ modeling.py
@@ -135,7 +135,8 @@ class BertModel(object):
                input_mask=None,
                token_type_ids=None,
                use_one_hot_embeddings=False,
-               scope=None):
+               scope=None,
+               reuse=None):
     """Constructor for BertModel.
 
     Args:
@@ -148,7 +149,8 @@ class BertModel(object):
       use_one_hot_embeddings: (optional) bool. Whether to use one-hot word
         embeddings or tf.embedding_lookup() for the word embeddings.
       scope: (optional) variable scope. Defaults to "bert".
-
+      reuse: (optional) bool, whether to reuse bert variables.
+        
     Raises:
       ValueError: The config is invalid or one of the input tensor shapes
         is invalid.
@@ -167,8 +169,8 @@ class BertModel(object):
 
     if token_type_ids is None:
       token_type_ids = tf.zeros(shape=[batch_size, seq_length], dtype=tf.int32)
-
-    with tf.variable_scope(scope, default_name="bert"):
+    
+    with tf.variable_scope(scope, default_name="bert", reuse=reuse):
       with tf.variable_scope("embeddings"):
         # Perform embedding lookup on the word ids.
         (self.embedding_output, self.embedding_table) = embedding_lookup(
