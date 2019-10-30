beb98a51da6f45418d3473f0ea6ed4ec6fbd7aba
wuwei <lxww301@gmail.com>
2019-10-30 19:07:36 +0800

    FEA add config

---
+++ run_pretraining.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+BERT_BASE_DIR=./config
+python3 run_pretraining.py \
+ --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+ --input_file=$STORAGE_BUCKET/data/tf_news_2016_zh_raw_news2016zh_1.tfrecord \
+ --output_dir=$STORAGE_BUCKET/bert_output/ \
+ --do_train=true \
+ --do_eval=true \
+ --use_tpu=true \
+ --vocab_file=$BERT_BASE_DIR/vocab.txt \
+ --do_lower_case=True \
+ --max_seq_length=512 \
+ --max_predictions_per_seq=51 \
+ --tpu=$TPU_NAME
