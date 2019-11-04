967567a42333a9a4dba80b44d012809336377f45
kinghuin <kinghuin_chull@163.com>
2019-11-04 15:14:23 +0800

    fix squad2 eval bug

--- run_squad.py
+++ run_squad.py
@@ -903,7 +903,9 @@ def write_predictions(all_examples, all_features, all_results, n_best_size,
       all_predictions[example.qas_id] = nbest_json[0]["text"]
     else:
       # predict "" iff the null score - the score of best non-null > threshold
-      score_diff = score_null - best_non_null_entry.start_logit - (
+      score_diff = score_null 
+      if best_non_null_entry:
+        score_diff -= best_non_null_entry.start_logit + (
           best_non_null_entry.end_logit)
       scores_diff_json[example.qas_id] = score_diff
       if score_diff > FLAGS.null_score_diff_threshold:
