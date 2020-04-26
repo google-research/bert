53b683e3fe9a6f6d1a7a600fb673b44ed44382bc
EthanChen <hchenecust@163.com>
2020-04-26 23:10:54 +0800

    update modeling.py

--- modeling.py
+++ modeling.py
@@ -195,8 +195,7 @@ class BertModel(object):
         # This converts a 2D mask of shape [batch_size, seq_length] to a 3D
         # mask of shape [batch_size, seq_length, seq_length] which is used
         # for the attention scores.
-        attention_mask = create_attention_mask_from_input_mask(
-            input_ids, input_mask)
+        attention_mask = create_attention_mask_from_input_mask(input_ids, input_mask)
 
         # Run the stacked transformer.
         # `sequence_output` shape = [batch_size, seq_length, hidden_size].
@@ -383,7 +382,8 @@ def embedding_lookup(input_ids,
                      use_one_hot_embeddings=False):
   """Looks up words embeddings for id tensor.
   Args:
-    input_ids: int32 Tensor of shape [batch_size, seq_length] containing word ids.  # [2, 3]
+    input_ids: int32 Tensor of shape [batch_size, seq_length] containing word ids.
+        # 输入为2维, [[31, 51, 99], [15, 5, 0]], shape = [2, 3]
     vocab_size: int. Size of the embedding vocabulary.
     embedding_size: int. Width of the word embeddings.
     initializer_range: float. Embedding initialization range.
@@ -395,20 +395,21 @@ def embedding_lookup(input_ids,
   """
   # This function assumes that the input is of shape [batch_size, seq_length, num_inputs].
   # If the input is a 2D tensor of shape [batch_size, seq_length], we reshape to [batch_size, seq_length, 1].
+  #
   if input_ids.shape.ndims == 2:
-    input_ids = tf.expand_dims(input_ids, axis=[-1])
+    input_ids = tf.expand_dims(input_ids, axis=[-1])  # [2, 3, 1]
   embedding_table = tf.get_variable(
       name=word_embedding_name,
       shape=[vocab_size, embedding_size],
       initializer=create_initializer(initializer_range))  # (32000, 128)
   flat_input_ids = tf.reshape(input_ids, [-1])
   if use_one_hot_embeddings:
-    one_hot_input_ids = tf.one_hot(flat_input_ids, depth=vocab_size)
+    one_hot_input_ids = tf.one_hot(flat_input_ids, depth=vocab_size)  # (6, 32000)
     output = tf.matmul(one_hot_input_ids, embedding_table)
   else:
-    output = tf.gather(embedding_table, flat_input_ids)
+    output = tf.gather(embedding_table, flat_input_ids)  # (6, 128)
 
-  input_shape = get_shape_list(input_ids)
+  input_shape = get_shape_list(input_ids)   # (2, 3, 1)
 
   output = tf.reshape(output, input_shape[0:-1] + [input_shape[-1] * embedding_size])  # (2, 3, 128)
   return (output, embedding_table)
@@ -425,24 +426,19 @@ def embedding_postprocessor(input_tensor,
                             max_position_embeddings=512,
                             dropout_prob=0.1):
   """Performs various post-processing on a word embedding tensor.
-
   Args:
-    input_tensor: float Tensor of shape [batch_size, seq_length,
-      embedding_size].
+    input_tensor: float Tensor of shape [batch_size, seq_length, embedding_size].
     use_token_type: bool. Whether to add embeddings for `token_type_ids`.
     token_type_ids: (optional) int32 Tensor of shape [batch_size, seq_length].
       Must be specified if `use_token_type` is True.
     token_type_vocab_size: int. The vocabulary size of `token_type_ids`.
-    token_type_embedding_name: string. The name of the embedding table variable
-      for token type ids.
+    token_type_embedding_name: string. The name of the embedding table variable for token type ids.
     use_position_embeddings: bool. Whether to add position embeddings for the
       position of each token in the sequence.
-    position_embedding_name: string. The name of the embedding table variable
-      for positional embeddings.
+    position_embedding_name: string. The name of the embedding table variable for positional embeddings.
     initializer_range: float. Range of the weight initialization.
-    max_position_embeddings: int. Maximum sequence length that might ever be
-      used with this model. This can be longer than the sequence length of
-      input_tensor, but cannot be shorter.
+    max_position_embeddings: int. Maximum sequence length that might ever be used with this model.
+        This can be longer than the sequence length of input_tensor, but cannot be shorter.
     dropout_prob: float. Dropout probability applied to the final output tensor.
 
   Returns:
@@ -451,7 +447,7 @@ def embedding_postprocessor(input_tensor,
   Raises:
     ValueError: One of the tensor shapes or input values is invalid.
   """
-  input_shape = get_shape_list(input_tensor, expected_rank=3)
+  input_shape = get_shape_list(input_tensor, expected_rank=3)  # (2, 3, 128)
   batch_size = input_shape[0]
   seq_length = input_shape[1]
   width = input_shape[2]
@@ -460,19 +456,17 @@ def embedding_postprocessor(input_tensor,
 
   if use_token_type:
     if token_type_ids is None:
-      raise ValueError("`token_type_ids` must be specified if"
-                       "`use_token_type` is True.")
+      raise ValueError("`token_type_ids` must be specified if" "`use_token_type` is True.")
     token_type_table = tf.get_variable(
         name=token_type_embedding_name,
         shape=[token_type_vocab_size, width],
-        initializer=create_initializer(initializer_range))
-    # This vocab will be small so we always do one-hot here, since it is always
-    # faster for a small vocabulary.
+        initializer=create_initializer(initializer_range))  # (16, 128)
+    # This vocab will be small so we always do one-hot here, since it is always faster for a small vocabulary.
     flat_token_type_ids = tf.reshape(token_type_ids, [-1])
-    one_hot_ids = tf.one_hot(flat_token_type_ids, depth=token_type_vocab_size)
-    token_type_embeddings = tf.matmul(one_hot_ids, token_type_table)
+    one_hot_ids = tf.one_hot(flat_token_type_ids, depth=token_type_vocab_size)  # (6, 16)
+    token_type_embeddings = tf.matmul(one_hot_ids, token_type_table)  # (6, 128)
     token_type_embeddings = tf.reshape(token_type_embeddings,
-                                       [batch_size, seq_length, width])
+                                       [batch_size, seq_length, width])  # (2, 3, 128)
     output += token_type_embeddings
 
   if use_position_embeddings:
@@ -481,7 +475,7 @@ def embedding_postprocessor(input_tensor,
       full_position_embeddings = tf.get_variable(
           name=position_embedding_name,
           shape=[max_position_embeddings, width],
-          initializer=create_initializer(initializer_range))
+          initializer=create_initializer(initializer_range))  # (512, 128)
       # Since the position embedding table is a learned variable, we create it
       # using a (long) sequence length `max_position_embeddings`. The actual
       # sequence length might be shorter than this, for faster training of
@@ -492,21 +486,21 @@ def embedding_postprocessor(input_tensor,
       # sequence has positions [0, 1, 2, ... seq_length-1], so we can just
       # perform a slice.
       position_embeddings = tf.slice(full_position_embeddings, [0, 0],
-                                     [seq_length, -1])
+                                     [seq_length, -1])  # (3, 128)
       num_dims = len(output.shape.as_list())
 
       # Only the last two dimensions are relevant (`seq_length` and `width`), so
       # we broadcast among the first dimensions, which is typically just
       # the batch size.
-      position_broadcast_shape = []
+      position_broadcast_shape = []  # (1, 3, 128)
       for _ in range(num_dims - 2):
         position_broadcast_shape.append(1)
       position_broadcast_shape.extend([seq_length, width])
       position_embeddings = tf.reshape(position_embeddings,
-                                       position_broadcast_shape)
+                                       position_broadcast_shape)  # (3, 128)
       output += position_embeddings
 
-  output = layer_norm_and_dropout(output, dropout_prob)
+  output = layer_norm_and_dropout(output, dropout_prob)  # (2, 3, 128)
   return output
 
 
@@ -976,62 +970,52 @@ def assert_rank(tensor, expected_rank, name=None):
 
 
 if __name__ == '__main__':
-    # vocab_size = 32000
-    # input_ids = tf.constant([[31, 51, 99], [15, 5, 0]])
-    # with tf.Session() as sess:
-    #     tf.global_variables_initializer().run()
-    #     embedding_lookup(vocab_size=vocab_size, input_ids=input_ids)
-
-    # test
-
-    VOCAB_SIZE = 2000  # 32000
-    EMBEDDING_SIZE = 5  # 128
-    USE_ONE_HOT_EMBEDDINGS = True
-
-    chars_input = tf.placeholder(dtype=tf.int32, shape=[None, None])
-    # 维度转换， [batch_size, seq_length, 1]
-    input_ids = chars_input
-    if chars_input.shape.ndims == 2:
-        input_ids = tf.expand_dims(chars_input, axis=[-1])
-
-    flat_input_ids = tf.reshape(input_ids, [-1])  # 1-D
-    embedding_table = tf.get_variable(name="word_embeddings",
-                                      shape=[VOCAB_SIZE, EMBEDDING_SIZE],
-                                      initializer=tf.truncated_normal_initializer(stddev=0.02))  # shape [4680, 100]
-    if USE_ONE_HOT_EMBEDDINGS:
-        one_hot_input_ids = tf.one_hot(flat_input_ids, depth=VOCAB_SIZE)  # len(flat_input_ids) x VOCAB_SIZE
-        output = tf.matmul(one_hot_input_ids, embedding_table)
-    else:
-        output = tf.gather(embedding_table, flat_input_ids)  # 类似于 embedding_lookup
-
-    input_shape = get_shape_list(input_ids)
-
+    vocab_size = 32000
+    input_ids = tf.constant([[31, 51, 99], [15, 5, 0]])
     with tf.Session() as sess:
         tf.global_variables_initializer().run()
-        print(sess.run(input_shape, feed_dict={chars_input: [[31, 51, 99], [15, 5, 0]]}))
+        output, embedding_table = embedding_lookup(vocab_size=vocab_size, input_ids=input_ids)
+        embedding_postprocessor(input_tensor=output,
+                                use_token_type=True,
+                                token_type_ids=[[0, 0, 1], [0, 2, 0]],
+                                token_type_vocab_size=16,
+                                token_type_embedding_name="token_type_embeddings",
+                                use_position_embeddings=True,
+                                position_embedding_name="position_embeddings",
+                                initializer_range=0.02,
+                                max_position_embeddings=512,
+                                dropout_prob=0.1)
 
-    # input_ids,
-    # vocab_size,
-    # embedding_size = 128,
-    # initializer_range = 0.02,
-    # word_embedding_name = "word_embeddings",
-    # use_one_hot_embeddings = False
-    # if input_ids.shape.ndims == 2:
-    #     input_ids = tf.expand_dims(input_ids, axis=[-1])
-    # embedding_table = tf.get_variable(
-    #     name=word_embedding_name,
-    #     shape=[vocab_size, embedding_size],
-    #     initializer=create_initializer(initializer_range))  # (32000, 128)
-    # flat_input_ids = tf.reshape(input_ids, [-1])
-    # if use_one_hot_embeddings:
-    #     one_hot_input_ids = tf.one_hot(flat_input_ids, depth=vocab_size)
+
+    # # test
+    # VOCAB_SIZE = 20  # 32000
+    # EMBEDDING_SIZE = 5  # 128
+    # USE_ONE_HOT_EMBEDDINGS = False
+    #
+    # chars_input = tf.placeholder(dtype=tf.int32, shape=[None, None])
+    # # 维度转换， [batch_size, seq_length, 1]
+    # input_ids = chars_input
+    # if chars_input.shape.ndims == 2:
+    #     input_ids = tf.expand_dims(chars_input, axis=[-1])
+    #
+    # flat_input_ids = tf.reshape(input_ids, [-1])  # 1-D
+    # embedding_table = tf.get_variable(name="word_embeddings",
+    #                                   shape=[VOCAB_SIZE, EMBEDDING_SIZE],
+    #                                   initializer=tf.truncated_normal_initializer(stddev=0.02))  # shape [4680, 100]
+    # if USE_ONE_HOT_EMBEDDINGS:
+    #     one_hot_input_ids = tf.one_hot(flat_input_ids, depth=VOCAB_SIZE)  # len(flat_input_ids) x VOCAB_SIZE
     #     output = tf.matmul(one_hot_input_ids, embedding_table)
     # else:
-    #     output = tf.gather(embedding_table, flat_input_ids)
+    #     output = tf.gather(embedding_table, flat_input_ids)  # 类似于 embedding_lookup
     #
-    # input_shape = get_shape_list(input_ids)
+    # # input_shape = get_shape_list(input_ids)  # 883
     #
-    # output = tf.reshape(output, input_shape[0:-1] + [input_shape[-1] * embedding_size])  # (2, 3, 128)
-    # return (output, embedding_table)
+    #
+    # with tf.Session() as sess:
+    #     tf.global_variables_initializer().run()
+    #     # print(sess.run(input_shape, feed_dict={chars_input: [[31, 51, 99], [15, 5, 0]]}))
+    #     print(sess.run(output, feed_dict={chars_input: [[8, 5, 0]]}))
+
+
 
 
