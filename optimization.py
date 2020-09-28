920ed4b90cd875d55cd72d5b7f97e5df759c71f3
abhilash1910 <debabhi1396@gmail.com>
2020-09-28 22:29:57 +0530

    Fix optimizer

--- optimization.py
+++ optimization.py
@@ -84,7 +84,7 @@ def create_optimizer(loss, init_lr, num_train_steps, num_warmup_steps, use_tpu):
   return train_op
 
 
-class AdamWeightDecayOptimizer(tf.train.Optimizer):
+class AdamWeightDecayOptimizer(tf.compat.v1.train.Optimizer):
   """A basic Adam optimizer that includes "correct" L2 weight decay."""
 
   def __init__(self,
